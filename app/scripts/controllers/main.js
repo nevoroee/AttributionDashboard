7debd4ea9d6eabeb5c0fd4ce61f97f5bd495134e
nevoroee <nevo.roee@gmail.com>
2014-02-09 18:40:08 +0200

    the line that keeps going missing is: <script src="scripts/lib/chosen/chosen.jquery.js"></script>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,34 +3,58 @@ angular.module('attributionDashboardApp')
   .controller('MainCtrl', function ($scope, $http, $q ,$filter) {
     $scope.campaigns = [];
     $scope.advertisers = [];
+    $scope.selectionGroups = [];
+    /*
+    function OnAjaxComplete(data){
+      $scope.$apply(function(){
+        $scope.advertisers = data;
+        for(var i = 0 ; i < $scope.advertisers.length ; i++ )
+        {
+          $scope.selectionGroups.push({
+              item:$scope.advertisers[i].Name,
+              group:"Advertsiers"}
+          )
+
+        }
+        console.log("I'm here");
+      });
+
+    };
+    */
+    $http.get('http://localhost:8000/advertisers')
+      .success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        //console.log(data);
+        $scope.advertisers = data;
+        for(var i = 0 ; i < $scope.advertisers.length ; i++ )
+        {
+          $scope.selectionGroups.push({
+              item:$scope.advertisers[i].Name,
+              group:"Advertsiers"}
+          )
+
+        }
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
     $http.get('http://localhost:8000/campaigns')
       .success(function(data, status, headers, config) {
         // this callback will be called asynchronously
         // when the response is available
         $scope.campaigns = data;
+        for(var i = 0 ; i < $scope.campaigns.length ; i++ )
+        {
+          $scope.selectionGroups.push({
+              item:$scope.campaigns[i].Name,
+              group:"Campaigns"}
+          )
+        }
       }).
       error(function(data, status, headers, config) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
       });
-    $http.get('http://localhost:8000/advertisers')
-      .success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(data);
-        $scope.advertisers = data;
-      }).
-      error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-
-    $scope.colors = [
-      {name:'black', shade:'dark'},
-      {name:'white', shade:'light'},
-      {name:'red', shade:'dark'},
-      {name:'blue', shade:'dark'},
-      {name:'yellow', shade:'light'}
-    ];
-    $scope.color = $scope.colors[2]; // red
   });
